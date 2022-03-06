f99f02a4f9bfb22bc5d6080563c7dfb290eaf57f
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-05 21:03:59 -0800

    code card model

--- DeckOfOneCard/Helpers/UIViewControllerExtension.swift
+++ DeckOfOneCard/Helpers/UIViewControllerExtension.swift
@@ -5,25 +5,15 @@
 //  Created by Mehdi MMS on 05/03/2022.
 //
 
-import UIKit
+import Foundation
 
-class UIViewControllerExtension: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+struct TopLevelObject: Decodable {
+    let cards: [Card]
 }
+
+struct Card: Decodable {
+    let image: URL
+    let value: String
+    let suit: String
+}
+
