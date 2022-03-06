ca21a8217798f1f6231ae33aacd97ebd5039c43a
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-05 20:56:48 -0800

    Initial Commit

---
+++ DeckOfOneCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeckOfOneCard
+//
+//  Created by Mehdi MMS on 05/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
