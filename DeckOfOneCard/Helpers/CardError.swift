54e12997ac535fad0d9b70c7ebf51b1475160b67
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-05 21:03:28 -0800

    add folder structure and initial files

---
+++ DeckOfOneCard/Helpers/CardError.swift
@@ -0,0 +1,8 @@
+//
+//  CardError.swift
+//  DeckOfOneCard
+//
+//  Created by Mehdi MMS on 05/03/2022.
+//
+
+import Foundation
---
+++ DeckOfOneCard/Model Controllers/CardController.swift
@@ -0,0 +1,8 @@
+//
+//  CardController.swift
+//  DeckOfOneCard
+//
+//  Created by Mehdi MMS on 05/03/2022.
+//
+
+import Foundation
